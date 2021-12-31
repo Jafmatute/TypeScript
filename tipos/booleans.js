ef77bf10ae295362db6c2f1bab5a86afd6a5ffac
Josue Ariel Flores Matute <josueari20@gmail.com>
2021-12-31 13:43:03 -0600

    Numbers - Números

--- tipos/booleans.js
+++ tipos/booleans.js
@@ -2,4 +2,7 @@
 (() => {
     let isSuperman = true;
     let isBatman = false;
+    isSuperman = true && false;
+    isSuperman = (isBatman) ? true : false;
+    console.log({ isSuperman });
 })();

f28ae1d669250e541e2a64fe36bf0e29bf02eb2a
Josue Ariel Flores Matute <josueari20@gmail.com>
2021-12-31 14:05:06 -0600

    Strings - Cadenas de caracteres

---
+++ tipos/strings.ts
@@ -0,0 +1,13 @@
+(()=> {
+
+const batman:string = 'Batman';
+const linternaVerde:string = 'Linterna verde';
+const panteraNegra:string = `Heroe: Pantera Negra`;
+
+console.log(`soy: ${batman}`)
+console.log(batman.toUpperCase());
+console.log(batman[10]?.toUpperCase() || 'No esta presente');
+
+
+
+})()

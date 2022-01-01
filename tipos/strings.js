b3d9aa18dffd8e869ec65fb2c2c97ebc1d8012c0
Josue Ariel Flores Matute <josueari20@gmail.com>
2021-12-31 20:10:07 -0600

    Tipo Any

--- tipos/strings.js
+++ tipos/strings.js
@@ -3,7 +3,7 @@
     var _a;
     const batman = 'Batman';
     const linternaVerde = 'Linterna verde';
-    const panteraNegra = `Hero: Pantera Negra`;
+    const panteraNegra = `Heroe: Pantera Negra`;
     console.log(`soy: ${batman}`);
     console.log(batman.toUpperCase());
     console.log(((_a = batman[10]) === null || _a === void 0 ? void 0 : _a.toUpperCase()) || 'No esta presente');

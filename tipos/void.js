b8b23dcc4808bfb965626005a129a8c83674a271
Josue Ariel Flores Matute <josueari20@gmail.com>
2021-12-31 20:53:19 -0600

    Void - Vacío

---
+++ tipos/void.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    function callBatman() {
+        return;
+    }
+    const callSuperman = () => {
+        return;
+    };
+    const batman = callBatman();
+    const superman = callSuperman();
+    console.log(batman);
+    console.log(superman);
+})();
---
+++ tipos/void.ts
@@ -0,0 +1,18 @@
+(() => {
+
+function callBatman(): void {
+return;
+}
+
+
+const callSuperman = (): void => {
+  return;
+}
+
+  const batman = callBatman();
+  const superman = callSuperman();
+
+  console.log(batman);
+  console.log(superman);
+
+})()

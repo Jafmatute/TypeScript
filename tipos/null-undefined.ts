5595fa8d9959e6f5926b97ec1a6b06396fa067ba
Josue Ariel Flores Matute <josueari20@gmail.com>
2021-12-31 21:14:18 -0600

    Null y Undefined

---
+++ tipos/null-undefined.ts
@@ -0,0 +1,13 @@
+(()=> {
+
+let nada:undefined = undefined;
+let nada2: number = undefined;
+
+// strickNullChecks
+// let isActive:(boolean|undefined) = undefined;
+let isActive:(boolean) = undefined;
+
+console.log(nada);
+
+
+})()

fda784d9d2f9e6821b9e7f58436577f1ec6c7855
Josue Ariel Flores <61173930+Jafmatute@users.noreply.github.com>
2022-01-02 11:40:16 -0600

    Delete tipos directory

--- tipos/null-undefined.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(() => {
-    let nada = undefined;
-    let nada2 = undefined;
-    // strickNullChecks
-    // let isActive:(boolean|undefined) = undefined;
-    let isActive = undefined;
-    console.log(nada);
-})();
--- tipos/tuples.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(() => {
-    const hero = ['dr strange', 100, false];
-    hero[0] = 'wolwerine';
-    hero[1] = 123;
-    hero[2] = true;
-    console.log(hero);
-})();

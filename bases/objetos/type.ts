bb42bf66b0a806162b6286b82ac51c3fc7275f5b
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-01-12 16:54:26 -0600

    Tipos personalizados

---
+++ bases/objetos/type.ts
@@ -0,0 +1,15 @@
+(() => {
+  type Hero = { name: string; age: number; powers: string[] };
+
+  let flash: Hero = {
+    name: "Barry Allen",
+    age: 24,
+    powers: ["Velocidad", "Viajar en el tiempo"],
+  };
+
+  let superman: Hero = {
+    name: "Clark ken",
+    age: 24,
+    powers: ["Velocidad", "fuerza"],
+  };
+})();

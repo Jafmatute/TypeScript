bba7fc15294ebab92e2504436968266ad770555a
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-01-15 14:31:39 -0600

    Funciones Genéricas

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,9 +1,14 @@
-import { printObject, genericFunction } from "./generics/generics";
+import {
+  printObject,
+  genericFunction,
+  genericFunctionArrow,
+} from "./generics/generics";
 
 /* printObject(123);
 printObject("Hola Mundo");
 printObject({ id: 123, name: "Hulk" });
 printObject([1, 2, 3, 4]); */
 
-console.log(genericFunction(3.1416));
-console.log(genericFunction(new Date()).toFixed(2));
+console.log(genericFunctionArrow(3.1416).toFixed(2));
+console.log(genericFunction("Hola").charAt(1));
+console.log(genericFunction(new Date()).getDay());

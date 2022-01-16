0da74fdbd52ed9ef1558b0aff7206bc3ba728dc7
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-01-15 18:41:51 -0600

    Decoradores de propiedades

--- ts-node/src/decorators/pokemon.ts
+++ ts-node/src/decorators/pokemon.ts
@@ -1,7 +1,13 @@
 function printToConsole(constructor: Function) {
-  console.log(constructor);
+  //console.log(constructor);
 }
 
+const bloquearPrototipo = function (constructor: Function) {
+  Object.seal(constructor);
+  Object.seal(constructor.prototype);
+};
+
+//Factory Decorators
 const printToConsoleConditional = (print: boolean = false): Function => {
   if (print) {
     return printToConsole;
@@ -10,7 +16,6 @@ const printToConsoleConditional = (print: boolean = false): Function => {
   return () => {};
 };
 
-//Factory Decorators
 function checkValidPokemonId() {
   return function (
     target: any,
@@ -29,14 +34,31 @@ function checkValidPokemonId() {
   };
 }
 
-const bloquearPrototipo = function (constructor: Function) {
-  Object.seal(constructor);
-  Object.seal(constructor.prototype);
-};
+function readOnly(isWritable: boolean = true): Function {
+  return function (target: any, propertyKey: string) {
+    const descriptor: PropertyDescriptor = {
+      get() {
+        console.log(this, "Getters");
+        return "Josue";
+      },
+      set(this, val) {
+        /* console.log(this, val); */
+        Object.defineProperty(this, propertyKey, {
+          value: val,
+          writable: !isWritable,
+          enumerable: false,
+        });
+      },
+    };
+
+    return descriptor;
+  };
+}
 
 @bloquearPrototipo
 @printToConsoleConditional(true)
 export class Pokemon {
+  @readOnly(true)
   public publicApi: string = `https://pokeapi.co`;
 
   constructor(public name: string) {}
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -3,5 +3,5 @@ import { Pokemon } from "./decorators/pokemon";
 const charmander = new Pokemon("Charmander");
 
 //(Pokemon.prototype as any).customName = 'Pikachu';
-
+charmander.publicApi = "www.gooogle.com";
 charmander.savePokemonToDB(1200);

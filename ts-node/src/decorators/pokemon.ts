09c58d5cac813b4297d296eca7fed8a8e670d677
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-01-15 18:10:40 -0600

    Decoradores de métodos

--- ts-node/src/decorators/pokemon.ts
+++ ts-node/src/decorators/pokemon.ts
@@ -10,6 +10,25 @@ const printToConsoleConditional = (print: boolean = false): Function => {
   return () => {};
 };
 
+//Factory Decorators
+function checkValidPokemonId() {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    console.log({ target, propertyKey, descriptor });
+    const originalMethod = descriptor.value;
+    descriptor.value = (id: number) => {
+      if (id < 1 || id > 800) {
+        return console.error(`el id del Pokemon debe estar entre 1 y 800`);
+      } else {
+        return originalMethod(id);
+      }
+    };
+  };
+}
+
 const bloquearPrototipo = function (constructor: Function) {
   Object.seal(constructor);
   Object.seal(constructor.prototype);
@@ -21,4 +40,9 @@ export class Pokemon {
   public publicApi: string = `https://pokeapi.co`;
 
   constructor(public name: string) {}
+
+  @checkValidPokemonId()
+  savePokemonToDB(id: number) {
+    console.log(`Pokemon guardado ${id}`);
+  }
 }
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -4,4 +4,4 @@ const charmander = new Pokemon("Charmander");
 
 //(Pokemon.prototype as any).customName = 'Pikachu';
 
-console.log(charmander);
+charmander.savePokemonToDB(1200);
